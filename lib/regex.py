c536c9b60fcd5f3e0e1daadc3819fb7e45b19930
Vivian Gichure <viviangichure@gmail.com>
2025-08-16 08:25:56 +0300

    completed lab

--- lib/regex.py
+++ lib/regex.py
@@ -1,13 +1,13 @@
 import re
 
-# NOTE: There are only a few tests included, so multiple solutions will work.
-# Feel free to encourage students to find oversights and add tests to this lab!
-
-name = r""
+# Name pattern: allows spaces, hyphens, and apostrophes with uppercase after apostrophe/hyphen/space
+name = r"^[A-Z][a-z]*(?:[ '-][A-Z][a-z]*)*$"
 name_regex = re.compile(name)
 
-phone_number = r""
+# Phone number pattern: matches 123-456-7890, (123) 456-7890, or 5555555555 (10 consecutive digits)
+phone_number = r'^(\d{10}|\(\d{3}\)\s?\d{3}-\d{4}|\d{3}-\d{3}-\d{4})$'
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+# Email pattern: must start with a letter, then letters, numbers, dots, underscores, or hyphens
+email_address = r'^[A-Za-z][\w\.-]*@[\w\.-]+\.\w{2,}$'
 email_regex = re.compile(email_address)
